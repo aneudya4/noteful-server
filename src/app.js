786c297525cee537e64b78029ba0ebfe9e96ffc1
Aneudy Adames <a4aneudy@gmail.com>
2021-05-27 01:58:13 -0400

    updated dburl

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(
 app.use(cors());
 app.use(helmet());
 
-app.use('/api/folders', () => {
+app.use('/api/folders', (req, res) => {
 	res.send('Hello, world!!!!');
 });
 
@@ -27,15 +27,15 @@ app.get('/', (req, res) => {
 	res.send('Hello, world!');
 });
 
-// app.use(function errorHandler(error, req, res, next) {
-// 	let response;
-// 	if (NODE_ENV === 'production') {
-// 		response = { error: 'Server error', env: NODE_ENV };
-// 	} else {
-// 		console.error(error);
-// 		response = { message: error.message, error };
-// 	}
-// 	res.status(500).json(response);
-// });
+app.use(function errorHandler(error, req, res, next) {
+	let response;
+	if (NODE_ENV === 'production') {
+		response = { error: 'Server error', env: NODE_ENV };
+	} else {
+		console.error(error);
+		response = { message: error.message, error };
+	}
+	res.status(500).json(response);
+});
 
 module.exports = app;
