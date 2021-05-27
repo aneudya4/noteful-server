bfcdb1ef722b8c1723e412b9fe73fea10f6a3184
Aneudy Adames <a4aneudy@gmail.com>
2021-05-27 01:28:10 -0400

    updated dburl

--- src/server.js
+++ src/server.js
@@ -6,8 +6,7 @@ const { PORT, DATABASE_URL } = require('./config');
 const db = knex({
 	client: 'pg',
 	connection:
-		'postgres://qdvzsdpdooqzzp:974beecc0b1ebe4332819af78fe365cdfe23f6e752ab74f495f0247460d2ad51@ec2-52-23-45-36.compute-1.amazonaws.com:5432/d6i50c86ofpfrp' +
-		'?ssl=true',
+		NODE_ENV === 'development' ? DATABASE_URL : DATABASE_URL + '?ssl=true',
 });
 
 app.set('db', db);
