260ca7011f5427940b29809e1bf6fe2c3d2de17b
Aneudy Adames <a4aneudy@gmail.com>
2021-05-27 14:31:50 -0400

    updated versions

--- src/server.js
+++ src/server.js
@@ -6,8 +6,7 @@ const { PORT, DATABASE_URL } = require('./config');
 const db = knex({
 	client: 'pg',
 	connection:
-		'postgres://atgylrivzwlqdl:a5f013a92f4e973b49db770bc6a271d98c3b77d76a2b3fae7a7ff34387b2adce@ec2-54-224-194-214.compute-1.amazonaws.com:5432/deiepo77t0pds8' +
-		'?ssl=true',
+		'postgres://atgylrivzwlqdl:a5f013a92f4e973b49db770bc6a271d98c3b77d76a2b3fae7a7ff34387b2adce@ec2-54-224-194-214.compute-1.amazonaws.com:5432/deiepo77t0pds8',
 });
 
 app.set('db', db);
